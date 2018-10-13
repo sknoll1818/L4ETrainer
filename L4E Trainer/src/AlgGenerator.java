7aa58fa1f6a3066a7db5a5cce619853fe963ad68
sknoll1818 <43442575+sknoll1818@users.noreply.github.com>
2018-10-12 20:10:55 -0400

    Update AlgGenerator.java

--- L4E Trainer/src/AlgGenerator.java
+++ L4E Trainer/src/AlgGenerator.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 
 public class AlgGenerator {
-    private static String[] slice5 = new String[] { "u", "e", "d", "E", "u'", "e'", "d'", "E'" };
+    private static String[] slice5 = new String[] { "u", "e", "d", "E", "u'", "e'", "d'", "E'", " " };
 
     private static String[] slice7 = new String[] { "u", "e", "d", "E", "2u", "2d", "3e", "u'", "e'", "d'", "E'", "2u'", "2d'",
             "3e'" };
@@ -13,11 +13,14 @@ public class AlgGenerator {
 
     public static String createAlg5(boolean parity) {
         String s = "";
-        for (int i = 0; i <= 2; i++) {
+        for (int i = 0; i <= 3; i++) {
             Random rand = new Random();
             int sliceR = rand.nextInt((6) + 1) + 0;
             int flipR = rand.nextInt(1 + 1) + 0;
-            if (sliceR >= 4) {
+            if (sliceR == 8) {
+            	s+= "E F2 E'";
+            }
+            else if (sliceR >= 4) {
                 s += slice5[sliceR] + flips[flipR] + " y ";
             } else
                 s += slice5[sliceR] + flips[flipR] + " y ";
@@ -29,17 +32,26 @@ public class AlgGenerator {
         return s;
     }
 
-    public static String createAlg7(boolean parity) {
+    public static String createAlg5w() {
         String s = "";
-        for (int i = 0; i <= 4; i++) {
+        for (int i = 0; i <= 3; i++) {
             Random rand = new Random();
-            int sliceR = rand.nextInt((13) + 1) + 0;
-            int flipR = rand.nextInt(2 + 1) + 0;
-            if (sliceR >= 6) {
-                s += slice7[sliceR] + flips[flipR] + " y ";
+            int sliceR = rand.nextInt((6) + 1) + 0;
+            int flipR = rand.nextInt(1 + 1) + 0;
+            if (sliceR == 8) {
+            	s+= "E F2 E'";
+            }
+            else if (sliceR >= 4) {
+                s += slice5[sliceR]+"w " + flips[flipR] + " y ";
             } else
-                s += slice7[sliceR] + flips[flipR] + " y ";
+                s += slice5[sliceR]+"w " + flips[flipR] + " y ";
         }
+
+        return s;
+    }
+
+    public static String createAlg7(boolean parity) {
+       String s = createAlg5w()+ createAlg5(parity);
         if (parity) {
             s += " z Rw U2 x Rw U2 Rw U2' Rw' U2 Lw U2 3Rw' U2' Rw U2 Rw' U2' Rw' R z' ";
         }
@@ -47,16 +59,7 @@ public class AlgGenerator {
     }
 
     public static String createAlg6(boolean parity) {
-        String s = "";
-        for (int i = 0; i <= 4; i++) {
-            Random rand = new Random();
-            int sliceR = rand.nextInt((10) + 1) + 0;
-            int flipR = rand.nextInt(1 + 1) + 0;
-            if (sliceR >= 5) {
-                s += slice6[sliceR] + flips[flipR] + " y ";
-            } else
-                s += slice6[sliceR] + flips[flipR] + " y ";
-        }
+    	 String s = createAlg5w()+ createAlg5(parity);
         if (parity) {
             s += " z Rw U2 x Rw U2 Rw U2' Rw' U2 Lw U2 3Rw' U2' Rw U2 Rw' U2' Rw' R z' ";
         }
