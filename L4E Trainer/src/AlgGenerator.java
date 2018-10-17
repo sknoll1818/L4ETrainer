d25925c780ce6670d0645ee0d30adba9d46f8a8f
sknoll1818 <43442575+sknoll1818@users.noreply.github.com>
2018-10-17 08:56:30 -0400

    Improved the scrambles

--- L4E Trainer/src/AlgGenerator.java
+++ L4E Trainer/src/AlgGenerator.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 
 public class AlgGenerator {
-    private static String[] slice5 = new String[] { "u", "e", "d", "E", "u'", "e'", "d'", "E'", " " };
+    private static String[] slice5 = new String[] { "u", "e", "d", "E", "u'", "e'", "d'", "E'", " ", "u2", "d2", "e2" };
 
     private static String[] slice7 = new String[] { "u", "e", "d", "E", "2u", "2d", "3e", "u'", "e'", "d'", "E'", "2u'", "2d'",
             "3e'" };
@@ -15,7 +15,7 @@ public class AlgGenerator {
         String s = "";
         for (int i = 0; i <= 3; i++) {
             Random rand = new Random();
-            int sliceR = (int)(Math.random() * 6 );
+            int sliceR = (int)(Math.random() * 11 );
             int flipR = (int)(Math.random() * 2 );
            switch(sliceR){
                case 0: s+= "u " + flips[flipR] + "u' y ";
@@ -30,8 +30,14 @@ public class AlgGenerator {
                    break;
                case 5: s+="d' " + flips[flipR] + "d y ";
                    break;
-               case 6: s+= "E F2 E'";
+               case 6: s+= "E F2 E' y ";
                    break;
+               case 7: s+= "u2 " +flips[flipR]+ "u2' y " ;
+               	break;
+               case 8: s+= "d2 " +flips[flipR]+ "d2' y " ;
+               		break;
+               case 9: s+= "e2 " +flips[flipR]+ "e2' y " ;
+               		break;
            }
         }
         if (parity) {
@@ -45,23 +51,31 @@ public class AlgGenerator {
         String s = "";
         for (int i = 0; i <= 3; i++) {
             Random rand = new Random();
-            int sliceR = (int)(Math.random() * 6 );
+            int sliceR = (int)(Math.random() * 11 );
             int flipR = rand.nextInt(1 + 1) + 0;
            switch(sliceR){
-               case 0: s+= "uw " + flips[flipR] + "uw' y ";
-                   break;
-               case 1: s+= "ew " + flips[flipR] + "ew' y ";
-                   break;
-               case 2: s+= "dw " + flips[flipR] + "dw' y ";
-                   break;
-               case 3: s+= "uw' " + flips[flipR] + "uw y ";
-                   break;
-               case 4: s+="ew' " + flips[flipR] + "ew y ";
-                   break;
-               case 5: s+="dw' " + flips[flipR] + "dw y ";
-                   break;
-               case 6: s+= "E F2 E'";
-                   break;
+
+           case 0: s+= "uw " + flips[flipR] + "uw' y ";
+               break;
+           case 1: s+= "ew " + flips[flipR] + "ew' y ";
+               break;
+           case 2: s+= "dw " + flips[flipR] + "dw' y ";
+               break;
+           case 3: s+= "uw' " + flips[flipR] + "uw y ";
+               break;
+           case 4: s+="ew' " + flips[flipR] + "ew y ";
+               break;
+           case 5: s+="dw' " + flips[flipR] + "dw y ";
+               break;
+           case 6: s+= "E F2 E' y ";
+               break;
+           case 7: s+= "uw2 " +flips[flipR]+ "uw2' y " ;
+           	break;
+           case 8: s+= "dw2 " +flips[flipR]+ "dw2' y " ;
+           		break;
+           case 9: s+= "ew2 " +flips[flipR]+ "ew2' y " ;
+           		break;
+       
            }
         }
         
